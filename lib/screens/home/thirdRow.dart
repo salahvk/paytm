a6ffe599b671e1c4d908d239f28a37cb5aab45b4
salah <salah@iocod.com>
2022-06-23 11:27:14 +0530

    draggable appbar and new rows added

---
+++ lib/screens/home/thirdRow.dart
@@ -0,0 +1,198 @@
+import 'package:flutter/material.dart';
+
+class ThirdRow extends StatelessWidget {
+  const ThirdRow({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+      child: Container(
+        width: double.infinity,
+        height: 130,
+        decoration: BoxDecoration(
+          color: const Color(0xFFEEEEEE),
+          borderRadius: BorderRadius.circular(5),
+          border: Border.all(
+            width: 0.5,
+          ),
+        ),
+        child: Row(mainAxisSize: MainAxisSize.max, children: [
+          Column(mainAxisSize: MainAxisSize.max, children: [
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Padding(
+                  padding: const EdgeInsetsDirectional.fromSTEB(6, 5, 0, 0),
+                  child: Text(
+                    'UPI & Wallet Payments',
+                    style: Theme.of(context).textTheme.bodyText1,
+                  ),
+                ),
+                Image.asset(
+                  'assets/images/upi_logo_icon_170312.webp',
+                  width: 10,
+                  height: 10,
+                  fit: BoxFit.cover,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 2, 10, 2),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      color: Color(0xFFEEEEEE),
+                    ),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding:
+                              const EdgeInsetsDirectional.fromSTEB(0, 5, 0, 5),
+                          child: Container(
+                            width: 40,
+                            height: 40,
+                            clipBehavior: Clip.antiAlias,
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                            ),
+                            child: Image.asset(
+                              'assets/images/scan and pay.PNG',
+                            ),
+                          ),
+                        ),
+                        Text(
+                          'Scan & Pay',
+                          maxLines: 1,
+                          style:
+                              Theme.of(context).textTheme.bodyText1!.copyWith(
+                                    fontFamily: 'Poppins',
+                                    fontSize: 10,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 2, 10, 2),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      color: Color(0xFFEEEEEE),
+                    ),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding:
+                              const EdgeInsetsDirectional.fromSTEB(0, 5, 0, 5),
+                          child: Container(
+                            width: 40,
+                            height: 40,
+                            clipBehavior: Clip.antiAlias,
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                            ),
+                            child: Image.asset(
+                              'assets/images/to mobile 2.PNG',
+                            ),
+                          ),
+                        ),
+                        Text(
+                          'To Mobile',
+                          style:
+                              Theme.of(context).textTheme.bodyText1!.copyWith(
+                                    fontFamily: 'Poppins',
+                                    fontSize: 10,
+                                  ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 2, 10, 2),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      color: Color(0xFFEEEEEE),
+                    ),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding:
+                              const EdgeInsetsDirectional.fromSTEB(0, 5, 0, 5),
+                          child: Container(
+                            width: 40,
+                            height: 40,
+                            clipBehavior: Clip.antiAlias,
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                            ),
+                            child: Image.asset(
+                              'assets/images/self.PNG',
+                            ),
+                          ),
+                        ),
+                        Text(
+                          'To Self',
+                          maxLines: 1,
+                          style:
+                              Theme.of(context).textTheme.bodyText1!.copyWith(
+                                    fontFamily: 'Poppins',
+                                    fontSize: 10,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 2, 10, 2),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      color: Color(0xFFEEEEEE),
+                    ),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding:
+                              const EdgeInsetsDirectional.fromSTEB(0, 5, 0, 5),
+                          child: Container(
+                            width: 40,
+                            height: 40,
+                            clipBehavior: Clip.antiAlias,
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                            ),
+                            child: Image.asset(
+                              'assets/images/to bank.PNG',
+                            ),
+                          ),
+                        ),
+                        Text(
+                          'To Bank A/c',
+                          maxLines: 1,
+                          style:
+                              Theme.of(context).textTheme.bodyText1!.copyWith(
+                                    fontFamily: 'Poppins',
+                                    fontSize: 10,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ])
+        ]),
+      ),
+    );
+  }
+}
